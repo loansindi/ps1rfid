4d8ce8e56ce9868ca0612fd6be13354808af6714
Derek Bever <derek.bever@gmail.com>
2014-09-25 17:28:53 -0500

    Very basic functionality. Next up - find a beaglebone

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+)
+
+import "fmt"
+
+func main() {
+	var code string
+	fmt.Scanf("%s", &code)
+	var request bytes.Buffer
+	request.WriteString("https://members.pumpingstationone.org/rfid/check/FrontDoor/")
+	request.WriteString(code)
+	resp, err := http.Get(request.String())
+	if err != nil {
+		fmt.Printf("Whoops!")
+	}
+	if resp.StatusCode == 200 {
+		fmt.Println("Success!")
+	} else if resp.StatusCode == 403 {
+		fmt.Println("Membership status: Expired")
+	} else {
+		fmt.Println("Code not found")
+	}
+
+}
