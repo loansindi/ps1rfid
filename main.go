ec8494c027aad7d0e081f49b865bce73e6b2670c
Derek Bever <derek.bever@gmail.com>
2014-09-25 21:49:49 -0500

    Make it loop

--- main.go
+++ main.go
@@ -10,22 +10,24 @@ import "fmt"
 
 func main() {
 	var code string
-	fmt.Print("Enter code: ")
-	fmt.Scanf("%s", &code)
-	var request bytes.Buffer
-	request.WriteString("https://members.pumpingstationone.org/rfid/check/FrontDoor/")
-	request.WriteString(code)
-	resp, err := http.Get(request.String())
-	if err != nil {
-		fmt.Printf("Whoops!")
-		os.Exit(1)
-	}
-	if resp.StatusCode == 200 {
-		fmt.Println("Success!")
-	} else if resp.StatusCode == 403 {
-		fmt.Println("Membership status: Expired")
-	} else {
-		fmt.Println("Code not found")
+	for {
+		fmt.Print("Enter code: ")
+		fmt.Scanf("%s", &code)
+		var request bytes.Buffer
+		request.WriteString("https://members.pumpingstationone.org/rfid/check/FrontDoor/")
+		request.WriteString(code)
+		resp, err := http.Get(request.String())
+		if err != nil {
+			fmt.Printf("Whoops!")
+			os.Exit(1)
+		}
+		if resp.StatusCode == 200 {
+			fmt.Println("Success!")
+		} else if resp.StatusCode == 403 {
+			fmt.Println("Membership status: Expired")
+		} else {
+			fmt.Println("Code not found")
+		}
 	}
 
 }
