962989ea7b70a13c5d7bfb4a2a532f410b8da3b9
Derek Bever <derek.bever@gmail.com>
2014-11-20 20:04:40 -0600

    update for id12 reader

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"github.com/mrmorphic/hwio"
+	//	"github.com/mrmorphic/hwio"
+	"github.com/tarm/goserial"
 	"net/http"
 	"os"
 )
@@ -11,24 +12,34 @@ import (
 var code string
 
 func main() {
-
+	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600}
+	u, err := serial.OpenPort(c)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
 	go http.HandleFunc("/", displayCode)
 	go http.ListenAndServe(":8080", nil)
+	buf := make([]byte, 16)
 	for {
-		fmt.Print("Enter code: ")
-		fmt.Scanf("%s", &code)
+		n, err := u.Read(buf)
+		if err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+		code := string(buf[1 : n-5])
 		var request bytes.Buffer
 		request.WriteString("https://members.pumpingstationone.org/rfid/check/FrontDoor/")
 		request.WriteString(code)
 		resp, err := http.Get(request.String())
-		defer hwio.CloseAll()
+		// defer hwio.CloseAll()
 		if err != nil {
 			fmt.Printf("Whoops!")
 			os.Exit(1)
 		}
 		if resp.StatusCode == 200 {
 			fmt.Println("Success!")
-			go openDoor()
+			//go openDoor()
 			code = ""
 		} else if resp.StatusCode == 403 {
 			fmt.Println("Membership status: Expired")
@@ -43,6 +54,7 @@ func displayCode(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(code))
 }
 
+/*
 func openDoor() {
 	strikePlate, err := hwio.GetPinWithMode("P9.11", hwio.OUTPUT)
 	if err != nil {
@@ -53,3 +65,4 @@ func openDoor() {
 	hwio.DigitalWrite(strikePlate, hwio.LOW)
 	hwio.ClosePin(strikePlate)
 }
+*/
