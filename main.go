eedeca7776e54eea3ad5eb3e001d43811c3cc65a
Derek Bever <derek.bever@gmail.com>
2014-11-22 20:31:22 -0600

    Remove spurious stuff

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"io"
-	"time"
 	"bytes"
 	"fmt"
-	"github.com/tarm/goserial"
-	"net/http"
-	"os"
 	"github.com/hybridgroup/gobot"
 	"github.com/hybridgroup/gobot/platforms/beaglebone"
 	"github.com/hybridgroup/gobot/platforms/gpio"
-
+	"github.com/tarm/goserial"
+	"io"
+	"net/http"
+	"os"
+	"time"
 )
 
 var code string
@@ -26,7 +25,6 @@ func main() {
 		os.Exit(1)
 	}
 	go http.HandleFunc("/", displayCode)
-//	go http.HandleFunc("/opensesame", openDoor)
 	go http.ListenAndServe(":8080", nil)
 	buf := make([]byte, 16)
 	for {
@@ -59,36 +57,8 @@ func main() {
 	}
 
 }
-/*
-func openDoor(w http.ResponseWriter, r *http.Request) {
-	splate.DigitalWrite(1)
-	gobot.After(5*time.Second, func() {
-		splate.DigitalWrite(0)
-})
-
-}
-*/
 
 func displayCode(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Code: "))
 	w.Write([]byte(code))
 }
-
-func openDoor() {
-
-	
-
-}
-/*
-func bot() {
-
-//	gbot := gobot.NewGobot()
-	
-	work := func() {
-		splate.DigitalWrite(1)
-		gobot.After(1*time.Second, func() {
-			splate.DigitalWrite(0)
-		})
-
-}}
-*/
