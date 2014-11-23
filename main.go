b9d4508e8715f8f4fddc5d0d4b27f86f5ef8903b
Derek Bever <derek.bever@gmail.com>
2014-11-22 20:14:58 -0600

    This version is working

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"io"
+	"time"
 	"bytes"
 	"fmt"
-	"github.com/mrmorphic/hwio"
 	"github.com/tarm/goserial"
 	"net/http"
 	"os"
+	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/platforms/beaglebone"
+	"github.com/hybridgroup/gobot/platforms/gpio"
+
 )
 
 var code string
 
 func main() {
+	beagleboneAdaptor := beaglebone.NewBeagleboneAdaptor("beaglebone")
+	splate := gpio.NewDirectPinDriver(beagleboneAdaptor, "splate", "P9_11")
 	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600}
 	u, err := serial.OpenPort(c)
 	if err != nil {
@@ -19,28 +26,31 @@ func main() {
 		os.Exit(1)
 	}
 	go http.HandleFunc("/", displayCode)
+//	go http.HandleFunc("/opensesame", openDoor)
 	go http.ListenAndServe(":8080", nil)
 	buf := make([]byte, 16)
 	for {
-		n, err := u.Read(buf)
+		n, err := io.ReadFull(u, buf)
 		if err != nil {
 			fmt.Print(err)
 			os.Exit(1)
 		}
-		code := string(buf[1 : n-5])
+		code = string(buf[1 : n-3])
 		var request bytes.Buffer
 		request.WriteString("https://members.pumpingstationone.org/rfid/check/FrontDoor/")
 		request.WriteString(code)
 		resp, err := http.Get(request.String())
-		defer hwio.CloseAll()
 		if err != nil {
 			fmt.Printf("Whoops!")
 			os.Exit(1)
 		}
 		if resp.StatusCode == 200 {
 			fmt.Println("Success!")
-			go openDoor()
 			code = ""
+			splate.DigitalWrite(1)
+			gobot.After(5*time.Second, func() {
+				splate.DigitalWrite(0)
+			})
 		} else if resp.StatusCode == 403 {
 			fmt.Println("Membership status: Expired")
 		} else {
@@ -49,18 +59,36 @@ func main() {
 	}
 
 }
+/*
+func openDoor(w http.ResponseWriter, r *http.Request) {
+	splate.DigitalWrite(1)
+	gobot.After(5*time.Second, func() {
+		splate.DigitalWrite(0)
+})
+
+}
+*/
 
 func displayCode(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Code: "))
 	w.Write([]byte(code))
 }
 
 func openDoor() {
-	strikePlate, err := hwio.GetPinWithMode("P9.11", hwio.OUTPUT)
-	if err != nil {
-		os.Exit(1)
-	}
-	hwio.DigitalWrite(strikePlate, hwio.HIGH)
-	hwio.Delay(500)
-	hwio.DigitalWrite(strikePlate, hwio.LOW)
-	hwio.ClosePin(strikePlate)
+
+	
+
 }
+/*
+func bot() {
+
+//	gbot := gobot.NewGobot()
+	
+	work := func() {
+		splate.DigitalWrite(1)
+		gobot.After(1*time.Second, func() {
+			splate.DigitalWrite(0)
+		})
+
+}}
+*/
